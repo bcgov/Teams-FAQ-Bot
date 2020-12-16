bb68804205b19e15d6916fbc8572f4e6c66af06c
Tariq Chatur <tariqchatur@gmail.com>
2020-12-16 14:54:12 -0800

    Removed Ask an Expert from the Tour Carosel

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/TourCarousel.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/TourCarousel.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
             return new List<Attachment>()
             {
                 GetCard(Strings.TeamNotificationHeaderText, Strings.TeamNotificationContent, appBaseUri + "/content/Notifications.png"),
-                GetCard(Strings.TeamChatHeaderText, Strings.TeamChatContent, appBaseUri + "/content/Enduserchat.png"),
+                //GetCard(Strings.TeamChatHeaderText, Strings.TeamChatContent, appBaseUri + "/content/Enduserchat.png"),
                 GetCard(Strings.TeamTicketSystemHeaderText, Strings.TeamTicketSystemContent, appBaseUri + "/content/Ticketsystem.png"),
             };
         }
@@ -37,7 +37,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
             return new List<Attachment>()
             {
                 GetCard(Strings.FunctionCardText1, Strings.FunctionCardText2, appBaseUri + "/content/Askaquestion.png"),
-                GetCard(Strings.AskAnExpertTitleText, Strings.AskAnExpertText2, appBaseUri + "/content/Expertinquiry.png"),
+                //GetCard(Strings.AskAnExpertTitleText, Strings.AskAnExpertText2, appBaseUri + "/content/Expertinquiry.png"),
                 GetCard(Strings.ShareFeedbackTitleText, Strings.FeedbackText1, appBaseUri + "/content/Sharefeedback.png"),
             };
         }
